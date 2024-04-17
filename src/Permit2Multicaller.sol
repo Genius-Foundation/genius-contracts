c64d3becf9183c107f19ccb62f090dd5d9f2be0f
lootboi <katora@dons.usfca.edu>
2024-04-17 13:51:43 -0600

    Updated Permit2Multicaller to use ISignatureTransfer.PermitBatchTransferFrom

--- src/Permit2Multicaller.sol
+++ src/Permit2Multicaller.sol
@@ -2,7 +2,7 @@
 pragma solidity ^0.8.4;
 
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
-import "permit2/interfaces/IAllowanceTransfer.sol";
+import "permit2/interfaces/ISignatureTransfer.sol";
 import "./GeniusVault.sol";
 
 /**
@@ -19,7 +19,7 @@ import "./GeniusVault.sol";
  */
 contract Permit2Multicaller {
 
-    IAllowanceTransfer public immutable PERMIT2;
+    ISignatureTransfer public immutable PERMIT2;
     GeniusVault public immutable VAULT;
     IERC20 public immutable STABLECOIN;
 
@@ -48,7 +48,7 @@ contract Permit2Multicaller {
 
     constructor(address _permit2, address _vault) payable {
 
-        PERMIT2 = IAllowanceTransfer(_permit2);
+        PERMIT2 = ISignatureTransfer(_permit2);
         VAULT = GeniusVault(_vault);
         STABLECOIN = IERC20(VAULT.STABLECOIN());
 
@@ -79,44 +79,88 @@ contract Permit2Multicaller {
 
 
     /**
-     * @dev Internal function to permit and transfer tokens from the caller's address.
-     * @param permitBatch The permit information for batch transfer.
-     * @param signature The signature for the permit.
-     */
-    function _permitAndBatchTransfer(
-        IAllowanceTransfer.PermitBatch calldata permitBatch, // permissions for the batch transfer
-        bytes calldata signature // signature for the permit from the owner
-    ) private {
-        if (permitBatch.spender != address(this)) {
-            revert InvalidSpender();
-        }
-        PERMIT2.permit(msg.sender, permitBatch, signature);
+    * @dev Facilitates the deposit of multiple types of tokens into the contract using a signed batch transfer permit.
+    * This function is orchestrated by a third party (orchestrator) who uses a permit signed by the token owner.
+    * The owner signs a permit authorizing the transfer of specific tokens in specific amounts, which the orchestrator
+    * then uses to execute the transfer, ensuring that token movements are fully authorized but not directly initiated by the token owner.
+    *
+    * Process Overview:
+    *
+    *     Token Owner ──────────────────────────────────┐
+    *         │                                         │
+    *         │ [Signs Permit]                          │
+    *         │     │                                   │
+    *         │     ▼                                   │
+    *     Orchestrator ─────────────────────────────────┼───────────┐
+    *         │                                         │           │
+    *         │ [Calls function with signature]         │           │
+    *         │     │                                   │           │
+    *         │     ▼                                   │           │ Permit2 Contract
+    *         │  [Signature Verified]                   │           │       │
+    *         │     │                                   │           │       │  [Batch Transfer Authorized]
+    *         │     └───────────────────────────────────┼───────────┼───────┘
+    *         │                                         │           │
+    *         │                                         │           │
+    *         │    [Batch Transfer Executed]            │           │
+    *         │       │                                 │           │
+    *         │       ▼                                 │           │
+    *         │    [Tokens Deposited into Contract]     │           │
+    *         │                                         │           │
+    *         └─────────────────────────────────────────┘           │
+    *                                                               │
+    *                                                               └────Tokens utilized by multicall contract─────►
+    *
+    * Key Steps:
+    * 1. The token owner generates a signature for a batch transfer, specifying the tokens and amounts they authorize to be transferred.
+    * 2. The orchestrator initiates the transaction by invoking this function, passing the owner's signature and the specified token details.
+    * 3. The function verifies the owner's signature via the Permit2 system, confirming that the transfer details match the owner's authorization.
+    * 4. Upon successful verification, the Permit2 contract executes the batch transfer of the specified tokens to this contract.
+    *
+    * @param _owner The address of the token owner who has signed the permit.
+    * @param _amounts Array of amounts for each token type, dictating how much of each token to transfer.
+    * @param _tokenAddresses Array of token addresses, specifying which tokens are to be transferred.
+    * @param _permit A signed PermitBatchTransferFrom structure containing the authorized transfers.
+    * @param _signature The digital signature from the token owner, proving authorization of the batch transfer.
+    */
+    function _permitAndTransferBatch(
+        address _owner,
+        uint256[] memory _amounts,
+        address[] memory _tokenAddresses,
+        ISignatureTransfer.PermitBatchTransferFrom calldata _permit,
+        bytes calldata _signature
+    ) internal {
+        if (_amounts.length == _tokenAddresses.length) revert ArrayLengthsMismatch();
+        if (_amounts.length == _permit.permitted.length) revert ArrayLengthsMismatch();
 
-        IAllowanceTransfer.AllowanceTransferDetails[] memory transferDetails = new IAllowanceTransfer.AllowanceTransferDetails[](
-            permitBatch.details.length
-        );
-        for (uint i = 0; i < permitBatch.details.length; ) {
-            transferDetails[i] = IAllowanceTransfer.AllowanceTransferDetails({
-                from: msg.sender,
+        uint256 len = _amounts.length;
+        ISignatureTransfer.SignatureTransferDetails[] memory details = new ISignatureTransfer.SignatureTransferDetails[](len);
+
+        for (uint256 i = 0; i < len;) {
+            // Permissions can only be granted to the multicall contract
+            details[i] = ISignatureTransfer.SignatureTransferDetails({
                 to: address(this),
-                amount: permitBatch.details[i].amount,
-                token: permitBatch.details[i].token
+                requestedAmount: _amounts[i]
             });
 
-            unchecked {
-                i++;
-            }
+            unchecked { ++i; }
         }
 
-        PERMIT2.transferFrom(transferDetails);
+        // Call Permit2 to perform the batch transfer using the provided signature
+        PERMIT2.permitTransferFrom(
+            _permit,    // The batch permit message
+            details,   // The details about each individual token transfer
+            _owner, // Owner of the tokens and signer of the message
+            _signature   // The packed signature from the token owner
+        );
     }
+
 
 
     /**
      * @dev Aggregates multiple calls in a single transaction.
      *      This method will set `sender` to the `msg.sender` temporarily
-     *      for the span of its execution.
-     *      This method does not support reentrancy.
+     *      for the span of its execution. Utilizes Permit2 for token transfers
+     *      to allow for batched token transfers without direct owner interaction.
      * @param targets An array of addresses to call.
      * @param data    An array of calldata to forward to the targets.
      * @param values  How much ETH to forward to each target.
@@ -126,11 +170,28 @@ contract Permit2Multicaller {
         address[] calldata targets, // routers
         bytes[] calldata data, // calldata
         uint256[] calldata values, // native 
-        IAllowanceTransfer.PermitBatch calldata permitBatch,
-        bytes calldata signature
+        ISignatureTransfer.PermitBatchTransferFrom calldata permitBatch, // permissions for the batch transfer
+        address owner, // owner of the tokens
+        bytes calldata signature // signature for the permit from the owner
     ) external payable returns (bytes[] memory) {
 
-        _permitAndBatchTransfer(permitBatch, signature);
+        uint256[] memory tokenAmounts = new uint256[](permitBatch.permitted.length);
+        address[] memory tokenAddresses = new address[](permitBatch.permitted.length);
+
+        for (uint256 i = 0; i < permitBatch.permitted.length;) {
+            tokenAmounts[i] = permitBatch.permitted[i].amount;
+            tokenAddresses[i] = permitBatch.permitted[i].token;
+
+            unchecked { ++i; }
+        }
+
+        _permitAndTransferBatch(
+            owner,
+            tokenAmounts,
+            tokenAddresses,
+            permitBatch,
+            signature
+        );
 
         assembly {
             if iszero(and(eq(targets.length, data.length), eq(data.length, values.length))) {
@@ -213,6 +274,9 @@ contract Permit2Multicaller {
         }
     }
 
+    /**
+     * @dev A contract that allows multiple calls to be aggregated and executed in a single transaction.
+     */
     function aggregateWithSender(
         address[] calldata targets, // routers
         bytes[] calldata data, // calldata
@@ -300,33 +364,86 @@ contract Permit2Multicaller {
         }
     }
 
-       /**
-     * @dev Aggregates multiple calls in a single transaction.
-     *      This method will set `sender` to the `msg.sender` temporarily
-     *      for the span of its execution.
-     *      This method does not support reentrancy.
-     * @param target An array of addresses to call.
-     * @param data    An array of calldata to forward to the targets.
-     * @param value  How much ETH to forward to each target.
-     * @param permitBatch The permit information for batch transfer.
-     * @param signature The signature for the permit.
-     * @param trader The address of the trader to deposit for.
-     */
-    function tokenSwapAndDeposit(
-        address target, // routers
-        bytes calldata data, // calldata
-        uint256 value, // native 
-        IAllowanceTransfer.PermitBatch calldata permitBatch,
+    /**
+    * @dev Executes multiple operations in a single transaction, combining token swapping,
+    *      permission verification, and liquidity deposit actions. This function is designed
+    *      to first transfer tokens based on a pre-authorized permit, swap tokens at a specified
+    *      address (router), and then deposit the swapped tokens into a liquidity pool.
+    * 
+    * Process Overview:
+    * 
+    *     Trader/Owner─────────────────────────────────┐
+    *        │                                         │
+    *        │ [Provides Signature for Batch Transfer] │
+    *        │       │                                 │    Permit2 Contract
+    *        │       ▼                                 │  [Verifies Signature]
+    *     Orchestrator───► Permit2Multicaller ─────────┼───────────┐
+    *        │                                         │           │
+    *        │       ┌─────────────────────────────────┼───────────┘
+    *        │       │                                 │           
+    *        │ [Initiates TokenSwapAndDeposit]         │           
+    *        │       │                                 │           
+    *        │       │ ────────┐                       │           
+    *        │       │ [Batch Transfer Tokens]         │           
+    *        │       │ ────────┘                       │           
+    *        │       │                                 │          
+    *        │       │                                 │        
+    *        │       │ ────────┐                       │        
+    *        │       │ [Swap Tokens at Target]         │           
+    *        │       │ ────────┘                       │          
+    *        │       │                                 │          
+    *        │       │ ────────┐                       │           
+    *        │       │ [Approve & Deposit into Vault]  │           
+    *        │       │ ────────┘                       │           
+    *        │       ▼                                 │           
+    *     Genius Vault ────────────────────────────────┘                                                                
+    *                                                             
+    *
+    * Key Steps:
+    * 1. A batch transfer is initiated using a signature from the owner for the specified tokens.
+    * 2. Tokens are then swapped at a target address.
+    * 3. The swapped tokens are deposited into the Genius Vault.
+    *
+    * @param target The address where the token swap will occur (e.g., a DEX router).
+    * @param data The calldata to be used for the swap operation at the target.
+    * @param value The amount of native tokens (e.g., ETH) to be sent with the call for purposes like gas fees.
+    * @param permitBatch The batch of permits detailing what tokens are allowed to be transferred by this contract.
+    * @param signature The digital signature from the token owner authorizing the batch transfer.
+    * @param owner The address of the token owner who is the signer and whose tokens are being swapped and deposited.
+    *
+    */
+    function singleSwapAndDeposit(
+        address target,
+        bytes calldata data,
+        uint256 value,
+        ISignatureTransfer.PermitBatchTransferFrom calldata permitBatch,
         bytes calldata signature,
-        address trader
+        address owner
     ) external payable {
-         _permitAndBatchTransfer(permitBatch, signature);
+
+        uint256[] memory tokenAmounts = new uint256[](permitBatch.permitted.length);
+        address[] memory tokenAddresses = new address[](permitBatch.permitted.length);
+
+        for (uint256 i = 0; i < permitBatch.permitted.length;) {
+            tokenAmounts[i] = permitBatch.permitted[i].amount;
+            tokenAddresses[i] = permitBatch.permitted[i].token;
+
+            unchecked { ++i; }
+        }
+        
+        _permitAndTransferBatch(
+            owner,
+            tokenAmounts,
+            tokenAddresses,
+            permitBatch,
+            signature
+        );
          
         assembly {
             sstore(returndatasize(), caller())
         }
 
-        address tokenToSwapAddress = permitBatch.details[0].token;
+        address tokenToSwapAddress = permitBatch.permitted[0].token;
         IERC20 tokenToSwap = IERC20(tokenToSwapAddress);
 
         uint256 amountToSwap = tokenToSwap.balanceOf(address(this));
@@ -339,13 +456,82 @@ contract Permit2Multicaller {
         uint256 amountToDeposit = STABLECOIN.balanceOf(address(this));
         require(STABLECOIN.approve(address(VAULT), amountToDeposit), "Approval failed");
 
-        VAULT.addLiquidity(trader, amountToDeposit);
+        VAULT.addLiquidity(owner, amountToDeposit);
 
         assembly {
             // Restore the `sender` slot.
             sstore(0, shl(160, 1))
         }
     }
+
+/**
+ * Executes multiple swap operations and deposits the swapped tokens into a liquidity pool.
+ * @param targets Array of router addresses for each token swap.
+ * @param data Array of calldata for each swap corresponding to the targets.
+ * @param values Array of native tokens (e.g., ETH) amounts to send with each swap call.
+ * @param permitBatch The permit information for batch transfer.
+ * @param signature The signature for the permit.
+ * @param owner The address of the trader to deposit for.
+ */
+function multiTokenSwapAndDeposit(
+    address[] calldata targets,
+    bytes[] calldata data,
+    uint256[] calldata values,
+    ISignatureTransfer.PermitBatchTransferFrom calldata permitBatch,
+    bytes calldata signature,
+    address owner
+) external payable {
+    if (
+    targets.length != data.length ||
+    targets.length != values.length ||
+    targets.length != permitBatch.permitted.length
+    ) revert ArrayLengthsMismatch();
+
+    uint256[] memory tokenAmounts = new uint256[](permitBatch.permitted.length);
+    address[] memory tokenAddresses = new address[](permitBatch.permitted.length);
+
+    for (uint256 i = 0; i < permitBatch.permitted.length;) {
+        tokenAmounts[i] = permitBatch.permitted[i].amount;
+        tokenAddresses[i] = permitBatch.permitted[i].token;
+        unchecked { ++i; }
+    }
+
+    _permitAndTransferBatch(
+        owner,
+        tokenAmounts,
+        tokenAddresses,
+        permitBatch,
+        signature
+    );
+
+    assembly {
+        // Set the sender slot temporarily for the span of this transaction.
+        sstore(returndatasize(), caller())
+    }
+
+    // Execute each swap and handle deposits
+    for (uint256 i = 0; i < targets.length;) {
+        IERC20 tokenToSwap = IERC20(tokenAddresses[i]);
+        uint256 amountToSwap = tokenToSwap.balanceOf(address(this));
+        require(tokenToSwap.approve(targets[i], amountToSwap), "Approval failed");
+
+        (bool success, ) = targets[i].call{value: values[i]}(data[i]);
+        require(success, "Swap call failed");
+
+        unchecked { ++i; }
+    }
+
+    // Deposit all received stablecoin into the liquidity pool
+    uint256 amountToDeposit = STABLECOIN.balanceOf(address(this));
+    require(STABLECOIN.approve(address(VAULT), amountToDeposit), "Approval failed");
+    VAULT.addLiquidity(owner, amountToDeposit);
+
+    assembly {
+        // Restore the `sender` slot.
+        sstore(0, shl(160, 1))
+    }
+}
+
 
 /**
  * @dev Simplified function to perform a single swap and then deposit stablecoins to a vault.
