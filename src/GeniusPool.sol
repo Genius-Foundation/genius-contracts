75426b03714c659a50783f398d72b5b419dfe1a1
lootboi <katora@dons.usfca.edu>
2024-04-22 21:22:31 -0600

    Refactored GeniusPool to track totalAssets, availableAssets, and totalStakedAssets

--- src/GeniusPool.sol
+++ src/GeniusPool.sol
@@ -28,9 +28,11 @@ contract GeniusPool is Orchestratable {
     //                          VARIABLES
     // =============================================================
 
-    uint256 public totalDeposits;
-    uint256 public totalStakedDeposits;
-    mapping(address => uint256) public isOrchestrator;
+    uint256 public totalAssets; // The total amount of stablecoin assets in the contract
+    uint256 public availableAssets; // totalAssets - (totalStakedAssets * (1 + rebalanceThreshold) (in percentage)
+    uint256 public totalStakedAssets; // The total amount of stablecoin assets made available to the pool through user deposits
+    uint256 public rebalanceThreshold = 10; // The maximum % of deviation from totalStakedAssets before blocking trades
+
     mapping(address => uint256) public stakedDeposits;
 
     // =============================================================
@@ -56,6 +58,11 @@ contract GeniusPool is Orchestratable {
      * @dev Error thrown when an invalid deposit token is encountered.
      */
      error InvalidDepositToken();
+
+    /**
+    * @dev Error thrown when the contract needs to be rebalanced.
+    */
+     error NeedsRebalance(uint256 totalStakedAssets, uint256 availableAssets);
 
     // =============================================================
     //                          EVENTS
@@ -117,7 +124,8 @@ contract GeniusPool is Orchestratable {
         if (_amount == 0) revert InvalidAmount();
 
         IERC20(STABLECOIN).transferFrom(tx.origin, address(this), _amount);
-        totalDeposits = STABLECOIN.balanceOf(address(this));
+        _updateBalance();
+        _updateAvailableAssets();
 
         emit ReceiveBridgeFunds(
             _amount,
@@ -142,6 +150,7 @@ contract GeniusPool is Orchestratable {
     ) public onlyOrchestrator payable {
         if (_amountIn == 0) revert InvalidAmount();
         if (_amountIn > STABLECOIN.balanceOf(address(this))) revert InvalidAmount();
+        if (!_isBalanceWithinThreshold(totalAssets - _amountIn)) revert NeedsRebalance(totalAssets, availableAssets);
 
         (
         uint256 fee,
@@ -164,13 +173,144 @@ contract GeniusPool is Orchestratable {
             bytes("") 
         );
 
-        totalDeposits = STABLECOIN.balanceOf(address(this));
+        _updateBalance();
+        _updateAvailableAssets();
 
         emit BridgeFunds(
             _amountIn,
             _dstChainId
         );
     }
+
+    // =============================================================
+    //                      SWAP LIQUIDITY
+    // =============================================================
+
+    /**
+     * @notice Deposits tokens into the vault
+     * @param _trader The address of the trader that tokens are being deposited for
+     * @param _amount The amount of tokens to deposit
+     */
+    function addLiquiditySwap(
+        address _trader,
+        uint256 _amount
+    ) external {
+        if (_trader == address(0)) revert InvalidTrader();
+        if (_amount == 0) revert InvalidAmount();
+
+        IERC20(STABLECOIN).transferFrom(msg.sender, address(this), _amount);
+        _updateBalance();
+        _updateAvailableAssets();
+
+        emit SwapDeposit(
+            _trader,
+            _amount
+        );
+    }
+
+    /**
+     * @notice Withdraws tokens from the vault
+     * @param _trader The address of the trader to use for 
+     * @param _amount The amount of tokens to withdraw
+     */
+    function removeLiquiditySwap(address _trader, uint256 _amount) external onlyOrchestrator {
+        if (_amount == 0) revert InvalidAmount();
+        if (_amount > totalAssets) revert InvalidAmount();
+        if (_trader == address(0)) revert InvalidTrader();
+        if (_amount > IERC20(STABLECOIN).balanceOf(address(this))) revert InvalidAmount();
+        if (!_isBalanceWithinThreshold(totalAssets - _amount)) revert NeedsRebalance(totalAssets, availableAssets);
+
+
+        IERC20(STABLECOIN).transfer(msg.sender, _amount);
+        _updateBalance();
+        _updateAvailableAssets();
+        
+        emit SwapWithdrawal(_trader, _amount);
+    }
+
+    // =============================================================
+    //                     REWARD LIQUIDITY
+    // =============================================================
+
+    function removeRewardLiquidity(uint256 _amount) external onlyOrchestrator {
+        if (_amount == 0) revert InvalidAmount();
+        if (_amount > totalAssets) revert InvalidAmount();
+        if (_amount > IERC20(STABLECOIN).balanceOf(address(this))) revert InvalidAmount();
+        if (!_isBalanceWithinThreshold(totalAssets - _amount)) revert InvalidAmount();
+
+        IERC20(STABLECOIN).transfer(msg.sender, _amount);
+        _updateBalance();
+        _updateAvailableAssets();
+    }
+
+    // =============================================================
+    //                     STAKING LIQUIDITY
+    // =============================================================
+
+    /**
+     * @dev Allows a user to stake liquidity tokens.
+     * @param _amount The amount of liquidity tokens to stake.
+     * @notice The `_amount` parameter must be greater than 0.
+     * @notice The function transfers the specified amount of liquidity tokens from the caller to the contract.
+     * @notice After the transfer, the function updates the `totalAssets` variable with the balance of liquidity tokens held by the contract.
+     */
+    function stakeLiquidity(address _trader, uint256 _amount) external {
+        if (_amount == 0) revert InvalidAmount();
+
+        IERC20(STABLECOIN).transferFrom(msg.sender, address(this), _amount);
+        _updateBalance();
+
+        stakedDeposits[_trader] += _amount;
+        totalStakedAssets += _amount;
+
+        _updateAvailableAssets();
+
+        emit Stake(
+            _trader,
+            _amount,
+            stakedDeposits[_trader] + _amount
+        );
+    }
+
+    /**
+     * @dev Removes staked liquidity from the GeniusPool contract.
+     * @param _amount The amount of liquidity to be removed.
+     * @notice The `_amount` must be greater than zero, less than or equal to the current deposits, and less than or equal to the balance of the STABLECOIN token in the contract.
+     * @notice Transfers the specified `_amount` of STABLECOIN tokens to the caller's address.
+     * @notice Updates the current deposits by getting the updated balance of the STABLECOIN token in the contract.
+     * @notice Throws an exception if any of the conditions are not met.
+     */
+    function removeStakedLiquidity(address _trader, uint256 _amount) external {
+        if (_trader != tx.origin) revert InvalidTrader();
+        if (_trader == address(0)) revert InvalidTrader();
+        if (_amount == 0) revert InvalidAmount();
+        if (_amount > totalAssets) revert InvalidAmount();
+        if (_amount > stakedDeposits[_trader]) revert InvalidAmount();
+        if (_amount > IERC20(STABLECOIN).balanceOf(address(this))) revert InvalidAmount();
+        if (!_isBalanceWithinThreshold(totalAssets - _amount, _amount)) revert NeedsRebalance(totalAssets, availableAssets);
+
+        IERC20(STABLECOIN).transfer(msg.sender, _amount);
+        _updateBalance();
+
+        stakedDeposits[_trader] -= _amount;
+        totalStakedAssets -= _amount;
+
+        _updateAvailableAssets();
+
+        emit Unstake(
+            _trader,
+            _amount,
+            stakedDeposits[_trader] - _amount
+        );
+    }
+
+    function setRebalanceThreshold(uint256 _threshold) external onlyOwner {
+        rebalanceThreshold = _threshold;
+    }
+
+    // =============================================================
+    //                     READ FUNCTIONS
+    // =============================================================
 
     function layerZeroFee(
         uint16 _chainId
@@ -195,113 +335,41 @@ contract GeniusPool is Orchestratable {
         return (_fee, _lzTxParams);
     }
 
-
     // =============================================================
-    //                      SWAP LIQUIDITY
+    //                     INTERNAL FUNCTIONS
     // =============================================================
 
-    /**
-     * @notice Deposits tokens into the vault
-     * @param _trader The address of the trader that tokens are being deposited for
-     * @param _amount The amount of tokens to deposit
-     */
-    function addLiquiditySwap(
-        address _trader,
-        uint256 _amount
-    ) external {
-        if (_trader == address(0)) revert InvalidTrader();
-        if (_amount == 0) revert InvalidAmount();
+    // balance is the current balance of the stable coin in the contract
+    function _isBalanceWithinThreshold(uint256 balance) public view returns (bool) {
+        uint256 lowerBound = (totalStakedAssets * rebalanceThreshold) / 100;
 
-        IERC20(STABLECOIN).transferFrom(msg.sender, address(this), _amount);
-        totalDeposits = STABLECOIN.balanceOf(address(this));
-
-        emit SwapDeposit(
-            _trader,
-            _amount
-        );
+        return balance >= lowerBound;
     }
 
-    /**
-     * @notice Withdraws tokens from the vault
-     * @param _trader The address of the trader to use for 
-     * @param _amount The amount of tokens to withdraw
-     */
-    function removeLiquiditySwap(address _trader, uint256 _amount) external onlyOrchestrator {
-        if (_amount == 0) revert InvalidAmount();
-        if (_amount > totalDeposits) revert InvalidAmount();
-        if (_trader == address(0)) revert InvalidTrader();
-        if (_amount > IERC20(STABLECOIN).balanceOf(address(this))) revert InvalidAmount();
+    function _isBalanceWithinThreshold(uint256 balance, uint256 amountToUnstake) internal view returns (bool) {
+        uint256 lowerBound = ((totalStakedAssets - amountToUnstake) * rebalanceThreshold) / 100;
 
+        return balance >= lowerBound;
+    }
 
-        IERC20(STABLECOIN).transfer(msg.sender, _amount);
-        totalDeposits = STABLECOIN.balanceOf(address(this));
+    function _updateBalance() internal {
+        totalAssets = STABLECOIN.balanceOf(address(this));
+    }
+
+    function _updateAvailableAssets() internal {
+        // Calculate the amount that is the threshold percentage of the staked assets
+        uint256 reduction = (totalStakedAssets * rebalanceThreshold) / 100;
+
+        // Calculate the liquidity needed as the staked assets minus the reduction
+        // Ensure not to underflow; if reduction is somehow greater, set neededLiquidity to 0
+        uint256 neededLiquidity = totalStakedAssets > reduction ? totalStakedAssets - reduction : 0;
         
-        emit SwapWithdrawal(_trader, _amount);
+        // Ensure we do not underflow when calculating available assets
+        if (totalAssets > neededLiquidity) {
+            availableAssets = totalAssets - neededLiquidity;
+        } else {
+            availableAssets = 0;
+        }
     }
 
-    // =============================================================
-    //                     REWARD LIQUIDITY
-    // =============================================================
-
-    function removeRewardLiquidity(uint256 _amount) external onlyOrchestrator {
-        if (_amount == 0) revert InvalidAmount();
-        if (_amount > totalDeposits) revert InvalidAmount();
-        if (_amount > IERC20(STABLECOIN).balanceOf(address(this))) revert InvalidAmount();
-
-        IERC20(STABLECOIN).transfer(msg.sender, _amount);
-        totalDeposits = STABLECOIN.balanceOf(address(this));
-    }
-
-    // =============================================================
-    //                     STAKING LIQUIDITY
-    // =============================================================
-
-    /**
-     * @dev Allows a user to stake liquidity tokens.
-     * @param _amount The amount of liquidity tokens to stake.
-     * @notice The `_amount` parameter must be greater than 0.
-     * @notice The function transfers the specified amount of liquidity tokens from the caller to the contract.
-     * @notice After the transfer, the function updates the `totalDeposits` variable with the balance of liquidity tokens held by the contract.
-     */
-    function stakeLiquidity(address _trader, uint256 _amount) external {
-        if (_amount == 0) revert InvalidAmount();
-
-        IERC20(STABLECOIN).transferFrom(msg.sender, address(this), _amount);
-        totalDeposits = STABLECOIN.balanceOf(address(this));
-
-        stakedDeposits[_trader] += _amount;
-        totalStakedDeposits += _amount;
-
-        emit Stake(
-            _trader,
-            _amount,
-            stakedDeposits[_trader] + _amount
-        );
-    }
-
-    /**
-     * @dev Removes staked liquidity from the GeniusPool contract.
-     * @param _amount The amount of liquidity to be removed.
-     * @notice The `_amount` must be greater than zero, less than or equal to the current deposits, and less than or equal to the balance of the STABLECOIN token in the contract.
-     * @notice Transfers the specified `_amount` of STABLECOIN tokens to the caller's address.
-     * @notice Updates the current deposits by getting the updated balance of the STABLECOIN token in the contract.
-     * @notice Throws an exception if any of the conditions are not met.
-     */
-    function removeStakedLiquidity(address _trader, uint256 _amount) external {
-        if (_amount == 0) revert InvalidAmount();
-        if (_amount > totalDeposits) revert InvalidAmount();
-        if (_amount > IERC20(STABLECOIN).balanceOf(address(this))) revert InvalidAmount();
-
-        IERC20(STABLECOIN).transfer(msg.sender, _amount);
-        totalDeposits = STABLECOIN.balanceOf(address(this));
-
-        stakedDeposits[_trader] -= _amount;
-        totalStakedDeposits -= _amount;
-
-        emit Unstake(
-            _trader,
-            _amount,
-            stakedDeposits[_trader] - _amount
-        );
-    }
 }
