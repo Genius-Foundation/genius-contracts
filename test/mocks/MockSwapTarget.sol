0a1856722fbd29d6ac864fedce13a6c9d1755019
lootboi <katora@dons.usfca.edu>
2024-06-25 12:59:50 -0600

    Removed unnecessary events in MockSwapTarget

--- test/mocks/MockSwapTarget.sol
+++ test/mocks/MockSwapTarget.sol
@@ -5,10 +5,7 @@ import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import {console} from "forge-std/Test.sol";
 
 contract MockSwapTarget {
-    event SwapCalled(address tokenIn, address trader, address recipient, uint256 amountIn, address tokenOut, address poolAddress, uint256 amountOut);
-    event BalanceAndAllowance(address token, uint256 balance, uint256 allowance);
-    event TransferExecuted(address token, address from, address to, uint256 amount);
-
+    
     function mockSwap(
         address tokenIn,
         uint256 amountIn,
