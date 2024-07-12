0807db90f83b8cea1a8c9af05272e426ce0812eb
lootboi <katora@dons.usfca.edu>
2024-07-12 16:34:50 -0400

    Revised Executable to not utilize a constructor, and instead utilize a function to initialize the executor contract within it

--- src/access/Executable.sol
+++ src/access/Executable.sol
@@ -3,6 +3,7 @@
 
 pragma solidity ^0.8.20;
 
+import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
 import {GeniusExecutor} from "../GeniusExecutor.sol";
 
 /**
@@ -10,15 +11,10 @@ import {GeniusExecutor} from "../GeniusExecutor.sol";
  * @author altloot
  * @dev A contract for managing Genius Executor permissions.
  */
-abstract contract Executable {
+abstract contract Executable is Ownable {
 
     GeniusExecutor public EXECUTOR;
-
-    constructor (
-        address executor
-    ) {
-        EXECUTOR = GeniusExecutor(executor);
-    }
+    uint256 public isExecutable = 0;
 
     error NotExecutor(address orchestrator);
 
@@ -28,10 +24,22 @@ abstract contract Executable {
     }
 
     /**
+     * @dev Initializes the executor contract.
+     * @param executor The address of the executor contract.
+     */
+    function _initializeExecutor(address executor) internal onlyOwner {
+        require(isExecutable == 0, "Executor already initialized");
+        EXECUTOR = GeniusExecutor(executor);
+
+        isExecutable = 1;
+    }
+
+    /**
      * @dev Internal function to check if the caller is the GeniusExecutor contract.
      * @dev Throws a `NotExecutor` error if the caller is not the GeniusExectorContract.
      */
     function _checkExecutor() internal view virtual {
+        require(isExecutable == 1, "Executor not initialized");
         if (msg.sender != address(EXECUTOR)) revert NotExecutor(msg.sender);
     }
 }
