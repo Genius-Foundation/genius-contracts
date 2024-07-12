074dbdf453166fee028917035b81091691737436
lootboi <katora@dons.usfca.edu>
2024-07-11 23:27:47 -0400

    First implementation of the Executable contract

---
+++ src/access/Executable.sol
@@ -0,0 +1,38 @@
+
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.20;
+
+import "@openzeppelin/contracts/access/Ownable.sol";
+import { GeniusExecutor } from "../GeniusExecutor.sol";
+
+/**
+ * @title Executable
+ * @author altloot
+ * @dev A contract for managing Genius Executor permissions.
+ */
+abstract contract Executable {
+
+    GeniusExecutor public EXECUTOR;
+
+    constructor (
+        address executor
+    ) {
+        EXECUTOR = GeniusExecutor(executor);
+    }
+
+    error NotExecutor(address orchestrator);
+
+    modifier onlyExecutor() {
+            _checkExecutor();
+        _;
+    }
+
+    /**
+     * @dev Internal function to check if the caller is the GeniusExecutor contract.
+     * @dev Throws a `NotExecutor` error if the caller is not the GeniusExectorContract.
+     */
+    function _checkExecutor() internal view virtual {
+        if (msg.sender != address(EXECUTOR)) revert NotExecutor(msg.sender);
+    }
+}
