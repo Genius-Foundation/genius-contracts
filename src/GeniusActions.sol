34633aa9d2862d7f72e48525ccf6441f0623f195
samuel-videau <samuel.videau@efrei.net>
2024-08-21 11:52:01 +0200

    refactor: new genius actions structure

--- src/GeniusActions.sol
+++ src/GeniusActions.sol
@@ -13,11 +13,18 @@ import "@openzeppelin/contracts/access/Ownable.sol";
 
 contract GeniusActions is Ownable {
 
-    string[] private activeActions;
-    string[] private inactiveActions;
+    struct Action {
+        bytes32 label;
+        string ipfsHash;
+        bool active;
+    }
 
-    mapping(string => string) private actionTypeToIpfsHash;
-    mapping(string => string) private inactiveActionTypeToIpfsHash;
+    uint256 nextActionId = 1;
+    uint256 inactiveCount = 0;
+
+    mapping(uint256 => Action) private idToAction;
+    mapping(bytes32 => uint256) private hashToId;
+    mapping(bytes32 => uint256) private labelToId;
 
     constructor(address initialOwner) Ownable(initialOwner) {
         require(initialOwner != msg.sender, "Initial owner cannot be the contract deployer");
@@ -27,107 +34,124 @@ contract GeniusActions is Ownable {
      * @dev Adds a new action with the given IPFS hash.
      * @param ipfsHash The IPFS hash of the action.
      */
-    function addAction(string memory action, string memory ipfsHash) public onlyOwner {
-        require(bytes(actionTypeToIpfsHash[action]).length == 0, "Action already exists");
-        actionTypeToIpfsHash[action] = ipfsHash; // Add to mapping
-        activeActions.push(action); // Add to active actions array
+    function addAction(bytes32 actionLabel, string memory ipfsHash) public onlyOwner {
+        bytes32 actionHash = _getActionHashFromIpfsHash(ipfsHash);
+        require(labelToId[actionLabel] == 0, "Label already exists");
+        require(hashToId[actionHash] == 0, "IPFS hash already exists");
+        _newAction(actionLabel, actionHash, ipfsHash);
     }
 
-    /**
-     * @dev Removes an action from the `actionTypeToIpfsHash` array at the specified index.
-     * @param action The index of the action to be removed.
-     * @notice Only the contract owner can call this function.
-     * @dev Throws an error if the index is out of bounds.
-     */
-    function removeAction(string memory action) public onlyOwner {
-        require(bytes(actionTypeToIpfsHash[action]).length != 0, "Action does not exist");
+    function updateActionStatusByHash(bytes32 actionHash, bool active) public onlyOwner {
+        uint256 actionId = hashToId[actionHash];
+        require(actionId != 0, "Action does not exist");
+        require(idToAction[actionId].active != active, "Status is already set to this value");
         
-        inactiveActionTypeToIpfsHash[action] = actionTypeToIpfsHash[action];
-        
-        delete actionTypeToIpfsHash[action];
-        for (uint256 i = 0; i < activeActions.length;) {
-            if (keccak256(abi.encodePacked(activeActions[i])) == keccak256(abi.encodePacked(action))) {
-                activeActions[i] = activeActions[activeActions.length - 1];
-                activeActions.pop();
-                break;
-            }
+        if (active) {
+            inactiveCount--;
+        } else {
+            inactiveCount++;
+        }
 
-            unchecked {
-                i++;
+        idToAction[actionId].active = active;
+    }
+
+    function updateActionStatusByLabel(bytes32 actionLabel, bool active) public onlyOwner {
+        uint256 actionId = labelToId[actionLabel];
+        require(actionId != 0, "Action does not exist");
+        require(idToAction[actionId].active != active, "Status is already set to this value");
+        
+        if (active) {
+            inactiveCount--;
+        } else {
+            inactiveCount++;
+        }
+
+        idToAction[actionId].active = active;
+    }
+
+    function updateActionIpfsHashByHash(bytes32 actionHash, string memory newIpfsHash) public onlyOwner {
+        uint256 actionId = hashToId[actionHash];
+        require(actionId != 0, "Action does not exist");
+        
+        _updateActionIpfsHash(actionHash, actionId, newIpfsHash);
+    }
+
+    function updateActionIpfsHashByLabel(bytes32 actionLabel, string memory newIpfsHash) public onlyOwner {
+        uint256 actionId = labelToId[actionLabel];
+        require(actionId != 0, "Action does not exist");
+
+        bytes32 actionHash = _getActionHashFromIpfsHash(idToAction[actionId].ipfsHash);
+        
+        _updateActionIpfsHash(actionHash, actionId, newIpfsHash);
+    }
+
+    function getActionByIpfsHash(string memory _ipfsHash) public view returns (Action memory) {
+        return getActionByActionHash(_getActionHashFromIpfsHash(_ipfsHash));
+    }
+
+    function getActionByActionHash(bytes32 _actionHash) public view returns (Action memory) {
+        Action memory action = idToAction[hashToId[_actionHash]];
+        require(bytes(action.ipfsHash).length != 0, "Action does not exist");
+        return action;
+    }
+
+    function getActionByActionLabel(bytes32 _actionLabel) public view returns (Action memory) {
+        Action memory action = idToAction[labelToId[_actionLabel]];
+        require(bytes(action.ipfsHash).length != 0, "Action does not exist");
+        return action;
+    }
+
+    function getActiveActions() public view returns (Action[] memory) {
+        Action[] memory result = new Action[](activeCount());
+        uint256 activeIndex = 0;
+
+        for (uint256 i = 1; i < nextActionId; i++) {
+            Action memory action = idToAction[i];
+            if (action.active) {
+                result[activeIndex] = action;
+                activeIndex++;
             }
         }
 
-        inactiveActions.push(action);
+        return result;
     }
 
-    /**
-     * @dev Updates the IPFS hash for a specific action type.
-     * @param action The index of the action type to update.
-     * @param ipfsHash The new IPFS hash to set for the action type.
-     * @notice Only the contract owner can call this function.
-     * @dev Throws an error if the index is out of bounds.
-     */
-    function updateAction(string memory action, string memory ipfsHash) public onlyOwner {
-        require(bytes(actionTypeToIpfsHash[action]).length != 0, "Action does not exist");
-        actionTypeToIpfsHash[action] = ipfsHash;
+    function getInactiveActions() public view returns (Action[] memory) {
+        Action[] memory result = new Action[](inactiveCount);
+        uint256 inactiveIndex = 0;
+
+        for (uint256 i = 1; i < nextActionId; i++) {
+            Action memory action = idToAction[i];
+            if (!action.active) {
+                result[inactiveIndex] = action;
+                inactiveIndex++;
+            }
+        }
+
+        return result;
     }
 
-    /**
-     * @dev Retrieves the IPFS hash associated with the action at the specified index.
-     * @param action The index of the action.
-     * @return The IPFS hash of the action.
-     * @notice Throws an error if the index is out of bounds.
-     */
-    function getAction(string memory action) public view returns (string memory) {
-        require(bytes(actionTypeToIpfsHash[action]).length != 0, "Action does not exist");
-        return actionTypeToIpfsHash[action];
+    function activeCount() public view returns (uint256) {
+        return nextActionId - 1 - inactiveCount;
     }
 
-    /**
-     * @dev Retrieves the IPFS hash associated with the historical action at the specified index.
-     * @param action The index of the historical action.
-     * @return The IPFS hash of the historical action.
-     * @notice Throws an error if the index is out of bounds.
-     */
-    function getInactiveAction(string memory action) public view returns (string memory) {
-        require(bytes(inactiveActionTypeToIpfsHash[action]).length != 0, "Historical action does not exist");
-        return inactiveActionTypeToIpfsHash[action];
+    function _updateActionIpfsHash(bytes32 actionHash, uint256 actionId, string memory newIpfsHash) internal {
+        bytes32 newActionHash = _getActionHashFromIpfsHash(newIpfsHash);
+        require(hashToId[newActionHash] == 0, "New IPFS hash already exists");
+
+        hashToId[actionHash] = 0;
+        hashToId[newActionHash] = actionId;      
+        idToAction[actionId].ipfsHash = newIpfsHash;
     }
 
-    /**
-     * @dev Gets all of the names of the actions (unoptimized for gas efficiency)
-     * @return string[] An array of all the active action names.
-     */
-    function activeActionNames() public view returns (string[] memory) {
-        require(activeActions.length > 0, "No active actions");
-        return activeActions;
+    function _newAction(bytes32 label, bytes32 actionHash, string memory ipfsHash) internal {
+        idToAction[nextActionId] = Action(label, ipfsHash, true);
+        labelToId[label] = nextActionId;
+        hashToId[actionHash] = nextActionId;
+        nextActionId++;
     }
 
-    /**
-     * @dev Retrieves the IPFS hash associated with the historical action at the specified index.
-     * @return string[] An array of all the historical action names.
-     */
-    function inactiveActionNames() public view returns (string[] memory) {
-        require(inactiveActions.length > 0, "No historical actions");
-        return inactiveActions;
+    function _getActionHashFromIpfsHash(string memory ipfsHash) internal pure returns (bytes32) {
+        return keccak256(abi.encodePacked(ipfsHash));
     }
-
-    /**
-     * @dev Returns the active action name at the specified index.
-     * @return string The active action name.
-     */
-    function getActiveActionName(uint256 index) public view returns (string memory) {
-        require(index < activeActions.length, "Index out of bounds");
-        return activeActions[index];
-    }
-
-    /**
-     * @dev Returns the historical action name at the specified index.
-     * @return string The historical action name.
-     */
-    function getInactiveActionName(uint256 index) public view returns (string memory) {
-        require(index < inactiveActions.length, "Index out of bounds");
-        return inactiveActions[index];
-    }
-
 }
