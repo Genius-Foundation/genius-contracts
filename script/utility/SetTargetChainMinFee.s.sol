2ca86d8ec89c467f8466f2d67a631a798a39d884
samuel-videau <samuel.videau@efrei.net>
2025-02-16 16:48:05 +1100

    fix: update SetTargetChainMinFee script with hardcoded values for fee and chain IDs

--- script/utility/SetTargetChainMinFee.s.sol
+++ script/utility/SetTargetChainMinFee.s.sol
@@ -12,10 +12,21 @@ contract SetTargetChainMinFee is Script {
         uint256 deployerPrivateKey = vm.envUint("DEPLOYER_PRIVATE_KEY");
         vm.startBroadcast(deployerPrivateKey);
 
-        geniusVault = GeniusVault(vm.envAddress("GENIUS_VAULT_ADDRESS"));
-        uint256 targetChainId = vm.envUint("TARGET_CHAIN_ID");
-        uint256 minFee = vm.envUint("MIN_FEE");
-        address feeToken = vm.envAddress("FEE_TOKEN");
-        geniusVault.setTargetChainMinFee(feeToken, targetChainId, minFee);
+        geniusVault = GeniusVault(0x05167A214DBC6EBB561dABe07d78e542B1C372B2);
+        geniusVault.setTargetChainMinFee(
+            0x833589fCD6eDb6E08f4c7C32D4f71b54bdA02913,
+            43114,
+            100000
+        );
+        geniusVault.setTargetChainMinFee(
+            0x833589fCD6eDb6E08f4c7C32D4f71b54bdA02913,
+            42161,
+            100000
+        );
+        geniusVault.setTargetChainMinFee(
+            0x833589fCD6eDb6E08f4c7C32D4f71b54bdA02913,
+            56,
+            100000
+        );
     }
 }
