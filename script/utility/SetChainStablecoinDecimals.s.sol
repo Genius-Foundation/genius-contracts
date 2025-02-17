125e73ba05bed318f85d5b6cfb0bf7431c010f41
samuel-videau <samuel.videau@efrei.net>
2025-02-17 22:48:19 +1100

    feat: add script to set stablecoin decimals for target chains in GeniusVault

---
+++ script/utility/SetChainStablecoinDecimals.s.sol
@@ -0,0 +1,24 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
+
+import {Script, console} from "forge-std/Script.sol";
+
+import {GeniusVault} from "../../src/GeniusVault.sol";
+
+contract SetTargetChainMinFee is Script {
+    GeniusVault public geniusVault;
+
+    function run() external {
+        uint256 deployerPrivateKey = vm.envUint("DEPLOYER_PRIVATE_KEY");
+        vm.startBroadcast(deployerPrivateKey);
+
+        geniusVault = GeniusVault(vm.envAddress("GENIUS_VAULT_ADDRESS"));
+        geniusVault.setChainStablecoinDecimals(10, 6);
+        geniusVault.setChainStablecoinDecimals(1, 6);
+        geniusVault.setChainStablecoinDecimals(8453, 6);
+        geniusVault.setChainStablecoinDecimals(42161, 6);
+        geniusVault.setChainStablecoinDecimals(43114, 6);
+        geniusVault.setChainStablecoinDecimals(56, 18);
+        geniusVault.setChainStablecoinDecimals(1399811149, 6);
+    }
+}
